4d8ed76c2f53281f1a0e9589a0569ac928316e46
Trang03k <nguty406@mymail.unisa.edu.au>
2023-04-03 17:05:44 +0930

    Update car.py

--- Week7/car.py
+++ Week7/car.py
@@ -9,3 +9,23 @@ class Car:
         else:
             raise ValueError(f'Invalid top speed {self.__topSpeed}')
     
+    def getTopSpeed(self):
+        return self.__topSpeed
+    
+    def getSpeed(self):
+        return self.__speed
+    
+    def accelerate(self):
+        self.__speed += 10
+        if self.__speed > self.__topSpeed:
+            self.__speed is self.__topSpeed
+            raise ValueError(f'Cannot accelerate above top speed: {self.__topSpeed}')
+    
+    def decelerate(self):
+        self.__speed -= 10
+        if self.__speed < 0:
+            self.__speed = 0
+            raise ValueError('Cannot decelerate below zero')
+        
+    def __str__(self):
+        return f'Car going {self.__topSpeed} kmph'
