0b155a426387f29350d390558642e51c518cb8a9
Trang03k <nguty406@mymail.unisa.edu.au>
2023-04-03 23:38:38 +0930

    Update car.py
    tested car class

--- Week7/car.py
+++ Week7/car.py
@@ -1,11 +1,13 @@
+#Task 1
 class Car:
-    def __init__(self,topSpeed,speed):
+    def __init__(self,topSpeed):
         self.__topSpeed = topSpeed
+        self.setTopSpeed(topSpeed)
         self.__speed = 0
 
-    def setTopSpeed(self):
+    def setTopSpeed(self,topSpeed):
         if self.__topSpeed > 0:
-            return self.__topSpeed
+            self.__topSpeed = topSpeed
         else:
             raise ValueError(f'Invalid top speed {self.__topSpeed}')
     
@@ -18,14 +20,64 @@ class Car:
     def accelerate(self):
         self.__speed += 10
         if self.__speed > self.__topSpeed:
-            self.__speed is self.__topSpeed
-            raise ValueError(f'Cannot accelerate above top speed: {self.__topSpeed}')
+            self.__speed = self.__topSpeed
+            raise Exception(f'Cannot accelerate above top speed: {self.__topSpeed}')
     
     def decelerate(self):
         self.__speed -= 10
         if self.__speed < 0:
             self.__speed = 0
-            raise ValueError('Cannot decelerate below zero')
+            raise Exception('Cannot decelerate below zero')
         
     def __str__(self):
-        return f'Car going {self.__topSpeed} kmph'
+        return f'Car going {self.__speed}/{self.__topSpeed} kmph'
+
+
+#Task2
+
+#1
+car1 = Car(250)
+print(car1)
+#2
+#car2 = Car(-99)
+#print(car2)
+
+#3
+try:
+    car2 = Car(-99)
+    print(car2)
+except:
+    print('I caught an exception')
+
+#4
+#for i in range(30):
+#    car1.accelerate()
+#    print(car1)
+
+#5
+try:
+    for i in range(30):
+        car1.accelerate()
+        print(car1)
+except:
+    print('I caught an exception')
+
+#6
+#for i in range(30):
+#    car1.decelerate()
+#    print(car1)
+
+#7
+for i in range(30):
+    try: 
+        car1.decelerate()
+        print(car1)
+    except:
+        print('I caught an exception ')
+
+#8
+try:
+    topspeed = Car(float(input('Please enter a number:')))
+except: 
+    print('I caught an exception ')
+
